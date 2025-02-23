38820315f1a5d2cef2fd627c396ef6a2a321806b
yamina169 <rezguiyamina1692001@gamil.com>
2025-02-23 18:13:28 +0100

    update css

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector(".nav-links");
+menuIcon.onclick = () => {
+  navLinks.classList.toggle("active");
+};
